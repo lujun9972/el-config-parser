85d559e7889d8f5b98b8794b79426ae25ec3caa5
lujun9972 <lujun9972@gmail.com>
2016-04-26 20:19:49 +0800

    Copyright

--- config-parser.el
+++ config-parser.el
@@ -1,6 +1,6 @@
 ;;; config-parser.el --- a library for parsing config file
 
-;; Copyright (C) 2004-2015 Free Software Foundation, Inc.
+;; Copyright (C) 2004-2015 DarkSun <lujun9972@gmail.com>.
 
 ;; Author: DarkSun <lujun9972@gmail.com>
 ;; Created: 2016-01-06
